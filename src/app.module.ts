4f26a4cc4d9440668d9f24798dc6832db4f1a3d7
Itziar <itziar.perezb@gmail.com>
2023-11-18 22:45:10 +0100

    creacion modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ApiModule } from './api/api.module';
+import { StudentModule } from './student/student.module';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ApiModule, StudentModule, LessonModule],
   controllers: [AppController],
   providers: [AppService],
 })
